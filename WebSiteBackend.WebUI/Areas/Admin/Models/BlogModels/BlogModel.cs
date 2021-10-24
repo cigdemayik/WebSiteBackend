0ecbc26b3fdd247db3c366c5bbe004f3a9f01ff1
Çiğdem Ayık <79482217+cigdemayik@users.noreply.github.com>
2021-10-25 00:30:13 +0300

    aaa

--- WebSiteBackend.WebUI/Areas/Admin/Models/BlogModels/BlogModel.cs
+++ WebSiteBackend.WebUI/Areas/Admin/Models/BlogModels/BlogModel.cs
@@ -9,6 +9,7 @@ namespace WebSiteBackend.WebUI.Areas.Admin.Models.BlogModels
 {
     public class BlogModel
     {
+        public int Id { get; set; }
         public string Header { get; set; }
         public string Content { get; set; }
         public string ImageUrl { get; set; }
